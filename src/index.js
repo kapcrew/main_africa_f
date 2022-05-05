6df35e113ca19fde28cd8e23d5a48375ffc4dbfd
kolyanchi <81684130+kolyanchi@users.noreply.github.com>
2022-05-06 01:48:03 +0300

    add btn contract

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from "@apollo/client";
-const client = new ApolloClient({
-  uri: "https://net.ton.dev/graphql",
-  cache: new InMemoryCache(),
-});
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+   <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
