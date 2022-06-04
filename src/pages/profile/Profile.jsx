e2fede4759dc9309f8e643d0e8e2c9c824fd7859
kolyanchi <81684130+kolyanchi@users.noreply.github.com>
2022-06-04 14:38:03 +0300

    add profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./profile.css";
 import { Cards } from "../../components";
+import { Toaster, toast } from "react-hot-toast";
 import Loader from "../../components/loader/loader";
 import apiRequest from "../../api/apiRequest";
 import coverImage from "../../assets/coverImage.png";
-import { menuCard, iconDischarge } from "../../assets/icon";
+import {
+  menuCard,
+  iconDischarge,
+  profileDiscord,
+  profileTwitter,
+  profileInstagram,
+} from "../../assets/icon";
 import CardCollection from "../../components/cardCollection/CardCollection";
 import DropzoneLoaderFile from "../../components/dropzoneLoaderFile/DropzoneLoaderFile";
 
@@ -35,6 +42,19 @@ const Profile = () => {
   const [surname, setsurname] = useState();
   const [description, setdescription] = useState();
   const [iconPrif, seticonPrif] = useState();
+
+  const [Email, setEmail] = useState();
+  const [Instagram, setInstagram] = useState();
+  const [Twitter, setTwitter] = useState();
+  const [Discord, setDiscord] = useState();
+  const [Bio, setBio] = useState();
+  const [WebSite, setWebSite] = useState();
+  const [ReferalCodeInput, setReferalCodeInput] = useState();
+  const [ReferalCode, setReferalCode] = useState();
+
+  const [isDisabledReferalCodeInput, setisDisabledReferalCodeInput] =
+    useState(false);
+
   const getProfile = async () => {
     await apiRequest.get("/profile/get_profile/").then((response) => {
       console.log(response);
@@ -45,6 +65,21 @@ const Profile = () => {
       setsurname(response.data.surname);
       setdescription(response.data.description);
       seticonPrif(response.data.profilePicture);
+
+      setEmail(response.data.email);
+      setInstagram(response.data.instagram);
+      setTwitter(response.data.twitter);
+      setDiscord(response.data.discord);
+      setBio(response.data.bio);
+      setWebSite(response.data.website);
+      setReferalCodeInput(response.data.affiliate);
+      setReferalCode(response.data.refCode);
+
+      if (
+        !(response.data.affiliate === "xxx" || response.data.affiliate === "")
+      ) {
+        setisDisabledReferalCodeInput(true);
+      }
     });
   };
 
@@ -52,12 +87,32 @@ const Profile = () => {
     getProfile();
   }, []);
   const updateProfile = async () => {
-    await apiRequest.post("/profile/update_profile/", {
-      description: description,
+    if (ReferalCodeInput === "xxx") {
+      setReferalCodeInput("");
+    }
+    console.log("ReferalCodeInput", ReferalCodeInput);
+    const objectData = {
+      description: "description",
       name: name,
       surname: surname,
       profilePicture: imageFile,
-    });
+      affiliate: ReferalCodeInput,
+      email: Email,
+      instagram: Instagram,
+      twitter: Twitter,
+      discord: Discord,
+      bio: Bio,
+      website: WebSite,
+    };
+    if (ReferalCodeInput === "" || ReferalCodeInput === "xxx"){
+      delete objectData.affiliate;
+    }
+    await apiRequest
+      .post("/profile/update_profile/", objectData)
+      .catch(({ response: { data } }) => {
+        toast.error(data.error);
+        console.log("error", data);
+      });
     await getProfile();
     setmodalUpdateData(false);
   };
@@ -135,15 +190,76 @@ const Profile = () => {
   const [selectingTab, setselectingTab] = useState(1);
   return (
     <div className="content">
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          className: "toaster",
+        }}
+      />
       <div className="cover-image">
         <img src={coverImage} alt="" />
       </div>
       <div className="icon-profile">
         <img className="icon-profile-img" src={iconPrif} alt="" />
       </div>
+
       <div className="info-user">
+        <div className="ref-code">
+          <div className="ref-code__title">Referral code</div>
+          <div className="ref-code__code">{ReferalCode}</div>
+        </div>
         <div className="info-user__name-user">
           {profile.name} {profile.surname}
+          <div className="icons_media">
+            {Discord && (
+              <div
+                className="info-user__icon_media"
+                onClick={() => window.open(Discord, "_blank")}
+              >
+                {profileDiscord}
+              </div>
+            )}
+            {Twitter && (
+              <div
+                className="info-user__icon_media"
+                onClick={() => window.open(Twitter, "_blank")}
+              >
+                {profileTwitter}
+              </div>
+            )}
+            {Instagram && (
+              <div
+                className="info-user__icon_media"
+                onClick={() => window.open(Instagram, "_blank")}
+              >
+                {profileInstagram}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="info-user__email-website">
+          {Email && <div className="info-user__email">{Email}</div>}
+          {WebSite && Email && (
+            <div className="info-user__email-website-wall">
+              <svg
+                width="1"
+                height="27"
+                viewBox="0 0 1 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="0.5"
+                  x2="0.5"
+                  y2="27"
+                  stroke="#482B08"
+                  stroke-opacity="0.3"
+                />
+              </svg>
+            </div>
+          )}
+          {WebSite && <div className="info-user__website">{WebSite}</div>}
         </div>
         <div className="info-user__address-user">
           {"0:" + profile.walletId?.substring(0, 2)}...
@@ -163,11 +279,11 @@ const Profile = () => {
           </div>
           <div className="info-user__bnt-discharge">{iconDischarge}</div>
         </div>
-        <div className="info-user__description">
-          <div className="info-user__description_text">
-            {profile.description}
+        {Bio && (
+          <div className="info-user__description">
+            <div className="info-user__description_text">{Bio}</div>
           </div>
-        </div>
+        )}
         {modalUpdateData && (
           <div className="for-modal-creact-collection">
             <div className="modalUpdateData">
@@ -194,15 +310,16 @@ const Profile = () => {
                 </div>
 
                 <div className="modalUpdateData__block_data">
-                  <div className="modalUpdateData__title">Description</div>
+                  <div className="modalUpdateData__title">Bio</div>
                   <textarea
-                    onChange={(e) => setdescription(e.target.value)}
-                    value={description}
+                    onChange={(e) => setBio(e.target.value)}
+                    value={Bio}
                     type="text"
                     className="modalUpdateData__input_description"
                   />
                 </div>
                 <div className="modalUpdateData__block_data">
+                  <div className="modalUpdateData__title">Avatar</div>
                   <div className="modal-creact-collection__loader-image">
                     <DropzoneLoaderFile
                       className="dropzone-modal"
@@ -215,6 +332,68 @@ const Profile = () => {
                       }
                     />
                   </div>
+                </div>
+                <div className="modalUpdateData__block_name">
+                  <div className="modalUpdateData__block_data">
+                    <div className="modalUpdateData__title">Email</div>
+                    <input
+                      type="text"
+                      value={Email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="modalUpdateData__input_name"
+                    />
+                  </div>
+                  <div className="modalUpdateData__block_data">
+                    <div className="modalUpdateData__title">Instagram</div>
+                    <input
+                      type="text"
+                      value={Instagram}
+                      onChange={(e) => setInstagram(e.target.value)}
+                      className="modalUpdateData__input_name"
+                    />
+                  </div>
+                </div>
+                <div className="modalUpdateData__block_name">
+                  <div className="modalUpdateData__block_data">
+                    <div className="modalUpdateData__title">Twitter</div>
+                    <input
+                      type="text"
+                      value={Twitter}
+                      onChange={(e) => setTwitter(e.target.value)}
+                      className="modalUpdateData__input_name"
+                    />
+                  </div>
+                  <div className="modalUpdateData__block_data">
+                    <div className="modalUpdateData__title">Discord</div>
+                    <input
+                      type="text"
+                      value={Discord}
+                      onChange={(e) => setDiscord(e.target.value)}
+                      className="modalUpdateData__input_name"
+                    />
+                  </div>
+                </div>
+
+                <div className="modalUpdateData__block_data">
+                  <div className="modalUpdateData__title">WebSite</div>
+                  <input
+                    type="text"
+                    value={WebSite}
+                    onChange={(e) => setWebSite(e.target.value)}
+                    className="modalUpdateData__input"
+                  />
+                </div>
+                <div className="modalUpdateData__block_data">
+                  <div className="modalUpdateData__title">
+                    Enter someone else's referral code
+                  </div>
+                  <input
+                    type="text"
+                    disabled={isDisabledReferalCodeInput}
+                    value={ReferalCodeInput}
+                    onChange={(e) => setReferalCodeInput(e.target.value)}
+                    className="modalUpdateData__input"
+                  />
                 </div>
               </div>
               <div className="modalUpdateData__btns">
